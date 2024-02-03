Add point rotation tests. Fix bugs on coords tracking and dtype passing. Add ndarray and T methods and tests.

Geometry3D/geometry/point.py
@@ -39,9 +39,8 @@ class Point(object):
             raise TypeError("Point() takes one or three arguments, not {}"
                     .format(len(args)))
         self.x, self.y, self.z = unify_types(coords)
-        self.coords = (self.x, self.y, self.z)
+        self.coords = [self.x, self.y, self.z]
         get_main_logger().debug('Create %s' %(self.__repr__(),))
-
 
     def __repr__(self):
         return "Point({}, {}, {})".format(
@@ -77,33 +76,38 @@ class Point(object):
     def __setitem__(self, item, value):
         """set the i element of a Point"""
         setattr(self, "xyz"[item], value)
+        self.coords[item] = value
     
-    def __array__(self):
+    def __array__(self,dtype=None):
         """return the array of a Point"""
-        return np.array([self.x, self.y, self.z])
+        return np.array([self.x, self.y, self.z],dtype=dtype)
 
+    def __ndarray__(self,dtype=None,n=2):
+        """return the n-dimensional array of a Point"""
+        return self.__array__(dtype=dtype)[:,*[np.newaxis]*(n-1)].T
+
+    def T(self):
+        """Column vector form of the point"""
+        return np.array([[self.x, self.y, self.z]]).T
     def pv(self):
         """Return the position vector of the point."""
         return Vector(self.x, self.y, self.z)
 
     def move(self, v):
         """Return the point that you get when you move self by vector v, self is also moved"""
-        if isinstance(v,Vector):
-            self.x += v[0]
-            self.y += v[1]
-            self.z += v[2]
-            return Point(self.pv())
-        else:
-            raise NotImplementedError("The second parameter for move function must be Vector")
-        
+        v = Vector(v) # the original author enforced moving with a vector, this respects the intention
+        self[0] += v[0]
+        self[1] += v[1]
+        self[2] += v[2]
+        return Point(self.coords)
+
     def rotate(self, angle: float, axis: Vector, point: Self = None):
         """Rotate around axis by angle (in radians) using trimesh transformations module"""
         if not isinstance(axis,(Vector,list,tuple,np.ndarray)):
             raise NotImplementedError("The first parameter for rotate function must be Vector")
-        # if point is None:
-        #     point = Point.origin() # unnecesary because the function already handles the origin case through None  kwarg
+
         R = transformations.rotation_matrix(angle,axis.normalized(),point)
-        v = transformations.transform_points(self,R) # could use np.dot here but not sure if I need to use all 4 cols of R
+        v = np.dot(R,np.array([self.coords]).T).T[:,:3] # could use np.dot here but not sure if I need to use all 4 cols of R
         self.x, self.y, self.z = v[0],v[1],v[2]
         return Point(v[0],v[1],v[2]) # this return respects the spirit of the rest of methods, but I think it should return None
 

Geometry3D/utils/vector.py
@@ -95,8 +95,8 @@ class Vector(object):
     def __setitem__(self, item, value):
         self._v[item] = value
     
-    def __array__(self):
-        return np.array(self._v)
+    def __array__(self,dtype=None):
+        return np.array(self._v,dtype=dtype)
     
     def cross(self, other):
         r"""Calculates the cross product of two vectors, defined as

unit_tests/test_point.py
@@ -1,7 +1,12 @@
+import copy
+from math import sqrt
 import unittest
 import numpy as np
+from trimesh import transformations
 from itertools import combinations
-from Geometry3D import Point
+from Geometry3D import Point, Vector
+
+
 class PointTest(unittest.TestCase):
     def test_origin(self):
         self.assertEqual(
@@ -25,11 +30,15 @@ class PointTest(unittest.TestCase):
         otherp = Point(1, 2, 3)
         all_combs = combinations([refp, lst, tup, arr, otherp], 2)
         for a, b in all_combs:
+            if not isinstance(a, Point):
+                a = Point(a)
+            if not isinstance(b, Point):
+                b = Point(b)
             self.assertEqual(
                 a,
                 b,
             )
-        # missing check on coords attribute
+        # Not testing coord by coord because it's done in the equality test
     def test_repr(self):
         self.assertEqual(
             repr(Point(1, 2, 3)),
@@ -44,4 +53,80 @@ class PointTest(unittest.TestCase):
             hash(Point(1, 2, 3)),
             hash(Point(1, 2, 4)),
         )
+    def test_move(self):
+        p = Point(1, 2, 3)
+        v = np.array([1, 2, 3])
+        self.assertEqual(
+            p.move(v),
+            Point(2, 4, 6),
+        )
+        self.assertEqual(
+            p.move(-1*v),
+            Point(1, 2, 3),
+        )
+    
+    def test_rotate(self):
+        p = Point(1, 1, 1)
+        orig = Point.origin()
+        axis = Vector(1, 0, 0)
+        refp = np.array([[1,1,1]])
+        angle = np.pi/2
+        rot_mat = transformations.rotation_matrix(np.pi/2, axis,point=orig)
+        rotated = transformations.transform_points(p.__ndarray__(n=2), rot_mat)
+        ref_rotated = transformations.transform_points(refp, rot_mat)
+        self.assertEqual(
+            Point(np.squeeze(rotated)),
+            Point(np.squeeze(ref_rotated)),
+        )
+        neworig = Point(1, 2, 3)
+        p = Point(1, 1, 1)
+        refp = np.array([[1,1,1]])
+        rot_mat = transformations.rotation_matrix(np.pi/2, axis,point=neworig)
+        rotated = transformations.transform_points(p.__ndarray__(n=2), rot_mat)
+        ref_rotated = transformations.transform_points(refp, rot_mat)
+        self.assertEqual(
+            Point(np.squeeze(rotated)),
+            Point(np.squeeze(ref_rotated)),
+        )
+    
+    def test_distance(self):
+        p1 = Point(1, 1, 1)
+        p2 = Point(1, 2, 3)
+        self.assertAlmostEqual(
+            p1.distance(p2),
+            sqrt(1 + 4),
+        )
 
+    def test_array(self):
+        self.assertTrue(
+            np.allclose(
+                Point(1, 2, 3).__array__(),
+                np.array([1, 2, 3]),
+            )
+        )
+    def test_ndarray(self):
+        p = Point(1, 2, 3)
+        _p = np.array([[[1,2,3]]])
+        self.assertTupleEqual(
+            p.__ndarray__(n=3).shape,
+            _p.shape,
+        )
+        self.assertTrue(
+            np.allclose(
+                p.__ndarray__(n=2),
+                np.array([[1, 2, 3]]),
+            )
+        )
+
+    def test_T(self):
+        p = Point(1, 2, 3)
+        self.assertTrue(
+            np.allclose(
+                p.T(),
+                np.array([[1, 2, 3]]).T,
+            )
+        )
+        self.assertTupleEqual(
+            p.T().shape,
+            (3, 1),
+        )